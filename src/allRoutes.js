2c1aa17e625b60c9e864bc8934f004668c8b85d8
nostam <matsonho@gmail.com>
2021-01-25 17:06:15 +0100

    fix package.json and combine routes

--- src/allRoutes.js
+++ src/allRoutes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const PostsRouter = require("./services/posts");
-
+const experiencesRouter = require("./services/experiences");
+const profilesRoute = require("./services/profiles");
 router.use("/posts", PostsRouter);
-
+router.use("/profiles", profilesRoute);
+router.use("/experiences", experienceRoute);
 module.exports = router;
