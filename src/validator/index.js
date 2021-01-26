94c98c7df2082960fe93ff6aefad70423b5f9b08
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-01-26 15:56:26 +0100

    added validation for profile route

--- src/validator/index.js
+++ src/validator/index.js
@@ -1,13 +1,19 @@
 const { body } = require("express-validator");
 
 const validateExperience = [
-  body("role","tell your role").isString(),
-  body("company","tell your role").isString(),
-  body("description","tell your role").isString(),
-
-
+  body("role", "tell your role").isString(),
+  body("company", "tell your role").isString(),
+  body("description", "tell your role").isString(),
 ];
-const validateProfile = [];
+const validateProfile = [
+  body("name", "invalid name").isString(),
+  body("surname", "invalid surname").isString(),
+  body("email", "invalid email").isString(),
+  body("bio", "say something about you!").isString(),
+  body("title", "invalid title").isString(),
+  body("area", "invalid area").isString(),
+  body("username", "invalid username").isString()
+];
 
 const validatePost = [
   body("text", "share something with us!").isString(),
