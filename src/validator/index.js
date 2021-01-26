61ee1bf107c123921cbb16e3bd074cf190ab8abe
nostam <matsonho@gmail.com>
2021-01-26 16:30:38 +0100

    fix typo in posts route

--- src/validator/index.js
+++ src/validator/index.js
@@ -1,19 +1,15 @@
 const { body } = require("express-validator");
 
 const validateExperience = [
-  body("role","tell your role").isString(),
-  body("company","tell your role").isString(),
-  body("description","tell your role").isString(),
-
-
+  body("role", "tell your role").isString(),
+  body("company", "tell your role").isString(),
+  body("description", "tell your role").isString(),
 ];
 const validateProfile = [];
 
 const validatePost = [
   body("text", "share something with us!").isString(),
   body("username", "Invalid username").isAlphanumeric(),
-  body("user.id").isAlphanumeric(),
 ];
-
 
 module.exports = { validatePost, validateExperience, validateProfile };
