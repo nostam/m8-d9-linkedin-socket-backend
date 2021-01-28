6ac17685f810742827b9f314267bff56caa79e96
Loris Cuntreri <62082748+MarelGuy@users.noreply.github.com>
2021-01-28 13:42:34 +0100

    deleted a commented code

--- src/model/experiences.js
+++ src/model/experiences.js
@@ -4,13 +4,13 @@ const mongoose_csv = require("mongoose-csv");
 
 const ExperienceSchema = new Schema(
   {
-    role: {type:String,required:true},
-    company: {type:String,required:true},
-    startDate: {type:String,required:true},
-    description: {type:String,required:true},
-    area: {type:String,required:true},
+    role: { type: String, required: true },
+    company: { type: String, required: true },
+    startDate: { type: String, required: true },
+    description: { type: String, required: true },
+    area: { type: String, required: true },
 
-    image: {type:String,default:"https://picsum.photos/x400"},
+    image: { type: String, default: "https://picsum.photos/x400" },
     username: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "profiles",
@@ -20,16 +20,6 @@ const ExperienceSchema = new Schema(
     timestamps: true,
   }
 );
-// ExperienceSchema.static("addUser", async function (user) {
-//   console.log("is this my user", user);
-
-//   await ExperienceSchema.findOneAndUpdate(
-//     { _id: id },
-//     {
-//       $addToSet: { username: { user } },
-//     }
-//   );
-// });
 
 ExperienceSchema.plugin(mongoose_csv);
 
